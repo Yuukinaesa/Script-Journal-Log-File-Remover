db8a7c78210b42bfb92a667941b4fc7bf94bae07
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2022-10-31 23:26:08 +0700

    Update hapus-log.sh

--- hapus-log.sh
+++ hapus-log.sh
@@ -2,13 +2,13 @@
 while true # untuk melakukan perulangan
 do # untuk menginisiasi program
 journalctl --disk-usage # menampilkan informasi penggunaan disk dari semua berkas journalctl
-sleep 3s # jeda program selama 3 detik
+sleep 5s # jeda program selama 5 detik
 rm -fr /var/log/journal # menghapus journalctl log
-sleep 3s # jeda program selama 3 detik
-journalctl --vacuum-size=10M # ruang disk yang digunakan untuk menyimpan journalctl akan dibersihkan sampai berukuran berkisar 10 MB
-sleep 3s # jeda program selama 3 detik
+sleep 5s # jeda program selama 5 detik
+journalctl --vacuum-size=7M # ruang disk yang digunakan untuk menyimpan journalctl akan dibersihkan sampai berukuran berkisar 7 MB
+sleep 5s # jeda program selama 5 detik
 systemctl restart systemd-journald # restart journal
-sleep 3s # jeda program selama 3 detik
+sleep 5s # jeda program selama 5 detik
 journalctl --disk-usage # menampilkan size dari journal
-sleep 1m # jeda program selama 1 menit
+sleep 2m # jeda program selama 2 menit
 done # untuk mengakhiri program
